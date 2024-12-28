aweXpect/aweXpect.Testably

feat: initialize repository

Pipeline/Build.ApiChecks.cs
@@ -1,0 +1,27 @@
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+// ReSharper disable AllUnderscoreLocalParameterName
+
+namespace Build;
+
+partial class Build
+{
+	Target ApiChecks => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			Project project = Solution.Tests.aweXpect_Testably_Api_Tests;
+
+			DotNetTest(s => s
+				.SetConfiguration(Configuration == Configuration.Debug ? "Debug" : "Release")
+				.SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
+				.EnableNoBuild()
+				.SetResultsDirectory(TestResultsDirectory)
+				.CombineWith(cc => cc
+					.SetProjectFile(project)
+					.AddLoggers($"trx;LogFileName={project.Name}.trx")), completeOnFailure: true);
+		});
+}

Pipeline/Build.MutationTests.cs
@@ -1,0 +1,97 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+// ReSharper disable AllUnderscoreLocalParameterName
+
+namespace Build;
+
+partial class Build
+{
+	Target MutationTests => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			AbsolutePath toolPath = TestResultsDirectory / "dotnet-stryker";
+			AbsolutePath configFile = toolPath / "Stryker.Config.json";
+			toolPath.CreateOrCleanDirectory();
+
+			DotNetToolInstall(_ => _
+				.SetPackageName("dotnet-stryker")
+				.SetToolInstallationPath(toolPath));
+
+			Dictionary<Project, Project[]> projects = new()
+			{
+				{ Solution.aweXpect_Testably, UnitTestProjects }
+			};
+
+			foreach (KeyValuePair<Project, Project[]> project in projects)
+			{
+				string branchName = GitVersion.BranchName;
+				if (GitHubActions?.Ref.StartsWith("refs/tags/", StringComparison.OrdinalIgnoreCase) == true)
+				{
+					string version = GitHubActions.Ref.Substring("refs/tags/".Length);
+					branchName = "release/" + version;
+					Log.Information("Use release branch analysis for '{BranchName}'", branchName);
+				}
+				
+				string configText = $$"""
+				                      {
+				                      	"stryker-config": {
+				                      		"project-info": {
+				                      			"name": "github.com/aweXpect/aweXpect.Testably",
+				                      			"module": "{{project.Key.Name}}",
+				                      			"version": "{{branchName}}"
+				                      		},
+				                      		"test-projects": [
+				                      			{{string.Join(",\n\t\t\t", project.Value.Select(PathForJson))}}
+				                      		],
+				                      		"project": {{PathForJson(project.Key)}},
+				                      		"target-framework": "net8.0",
+				                      		"since": {
+				                      			"target": "main",
+				                      			"enabled": {{(GitVersion.BranchName != "main").ToString().ToLowerInvariant()}},
+				                      			"ignore-changes-in": [
+				                      				"**/.github/**/*.*"
+				                      			]
+				                      		},
+				                      		"reporters": [
+				                      			"html",
+				                      			"progress",
+				                      			"cleartext"
+				                      		],
+				                      		"mutation-level": "Advanced"
+				                      	}
+				                      }
+				                      """;
+				File.WriteAllText(configFile, configText);
+				Log.Debug($"Created '{configFile}':{Environment.NewLine}{configText}");
+
+				string arguments = IsServerBuild
+					? $"-f \"{configFile}\" -r \"Dashboard\" -r \"cleartext\""
+					: $"-f \"{configFile}\" -r \"cleartext\"";
+
+				string executable = EnvironmentInfo.IsWin ? "dotnet-stryker.exe" : "dotnet-stryker";
+				IProcess process = ProcessTasks.StartProcess(
+						Path.Combine(toolPath, executable),
+						arguments,
+						Solution.Directory)
+					.AssertWaitForExit();
+				if (process.ExitCode != 0)
+				{
+					Assert.Fail(
+						$"Stryker did not execute successfully for {project.Key.Name}: (exit code {process.ExitCode}).");
+				}
+			}
+		});
+
+	static string PathForJson(Project project) => $"\"{project.Path.ToString().Replace(@"\", @"\\")}\"";
+}

Source/aweXpect.Testably/Dummy.cs
@@ -1,0 +1,7 @@
+﻿namespace aweXpect.Testably;
+
+public class Dummy
+{
+	public int Double(int value)
+		=> value * 2;
+}

Tests/aweXpect.Testably.Api.Tests/ApiAcceptance.cs
@@ -1,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace aweXpect.Api.Tests;
+
+public sealed class ApiAcceptance
+{
+	/// <summary>
+	///     Execute this test to update the expected public API to the current API surface.
+	/// </summary>
+	[TestCase]
+	[Explicit]
+	public void AcceptApiChanges()
+	{
+		string[] assemblyNames =
+		[
+			"aweXpect.Testably"
+		];
+
+		foreach (string assemblyName in assemblyNames)
+		{
+			foreach (string framework in Helper.GetTargetFrameworks())
+			{
+				string publicApi = Helper.CreatePublicApi(framework, assemblyName)
+					.Replace("\n", Environment.NewLine);
+				Helper.SetExpectedApi(framework, assemblyName, publicApi);
+			}
+		}
+
+		Assert.That(assemblyNames, Is.Not.Empty);
+	}
+}

Tests/aweXpect.Testably.Api.Tests/ApiApprovalTests.cs
@@ -1,0 +1,38 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace aweXpect.Api.Tests;
+
+/// <summary>
+///     Whenever a test fails, this means that the public API surface changed.
+///     If the change was intentional, execute the <see cref="ApiAcceptance.AcceptApiChanges()" /> test to take over the
+///     current public API surface. The changes will become part of the pull request and will be reviewed accordingly.
+/// </summary>
+public sealed class ApiApprovalTests
+{
+	[TestCaseSource(typeof(TargetFrameworksTheoryData))]
+	public void VerifyPublicApiForAweXpectTestably(string framework)
+	{
+		const string assemblyName = "aweXpect.Testably";
+
+		string publicApi = Helper.CreatePublicApi(framework, assemblyName);
+		string expectedApi = Helper.GetExpectedApi(framework, assemblyName);
+
+		Assert.That(publicApi, Is.EqualTo(expectedApi));
+	}
+
+	private sealed class TargetFrameworksTheoryData : IEnumerable
+	{
+		#region IEnumerable Members
+
+		public IEnumerator GetEnumerator()
+		{
+			foreach (string targetFramework in Helper.GetTargetFrameworks())
+			{
+				yield return new object[] { targetFramework };
+			}
+		}
+
+		#endregion
+	}
+}

aweXpect/aweXpect.Chronology

Pipeline/Build.ApiChecks.cs
@@ -1,0 +1,27 @@
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+// ReSharper disable AllUnderscoreLocalParameterName
+
+namespace Build;
+
+partial class Build
+{
+	Target ApiChecks => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			Project project = Solution.Tests.aweXpect_Chronology_Api_Tests;
+
+			DotNetTest(s => s
+				.SetConfiguration(Configuration == Configuration.Debug ? "Debug" : "Release")
+				.SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
+				.EnableNoBuild()
+				.SetResultsDirectory(TestResultsDirectory)
+				.CombineWith(cc => cc
+					.SetProjectFile(project)
+					.AddLoggers($"trx;LogFileName={project.Name}.trx")), completeOnFailure: true);
+		});
+}

Pipeline/Build.MutationTests.cs
@@ -1,0 +1,97 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+// ReSharper disable AllUnderscoreLocalParameterName
+
+namespace Build;
+
+partial class Build
+{
+	Target MutationTests => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			AbsolutePath toolPath = TestResultsDirectory / "dotnet-stryker";
+			AbsolutePath configFile = toolPath / "Stryker.Config.json";
+			toolPath.CreateOrCleanDirectory();
+
+			DotNetToolInstall(_ => _
+				.SetPackageName("dotnet-stryker")
+				.SetToolInstallationPath(toolPath));
+
+			Dictionary<Project, Project[]> projects = new()
+			{
+				{ Solution.aweXpect_Chronology, UnitTestProjects }
+			};
+
+			foreach (KeyValuePair<Project, Project[]> project in projects)
+			{
+				string branchName = GitVersion.BranchName;
+				if (GitHubActions?.Ref.StartsWith("refs/tags/", StringComparison.OrdinalIgnoreCase) == true)
+				{
+					string version = GitHubActions.Ref.Substring("refs/tags/".Length);
+					branchName = "release/" + version;
+					Log.Information("Use release branch analysis for '{BranchName}'", branchName);
+				}
+				
+				string configText = $$"""
+				                      {
+				                      	"stryker-config": {
+				                      		"project-info": {
+				                      			"name": "github.com/aweXpect/aweXpect.Chronology",
+				                      			"module": "{{project.Key.Name}}",
+				                      			"version": "{{branchName}}"
+				                      		},
+				                      		"test-projects": [
+				                      			{{string.Join(",\n\t\t\t", project.Value.Select(PathForJson))}}
+				                      		],
+				                      		"project": {{PathForJson(project.Key)}},
+				                      		"target-framework": "net8.0",
+				                      		"since": {
+				                      			"target": "main",
+				                      			"enabled": {{(GitVersion.BranchName != "main").ToString().ToLowerInvariant()}},
+				                      			"ignore-changes-in": [
+				                      				"**/.github/**/*.*"
+				                      			]
+				                      		},
+				                      		"reporters": [
+				                      			"html",
+				                      			"progress",
+				                      			"cleartext"
+				                      		],
+				                      		"mutation-level": "Advanced"
+				                      	}
+				                      }
+				                      """;
+				File.WriteAllText(configFile, configText);
+				Log.Debug($"Created '{configFile}':{Environment.NewLine}{configText}");
+
+				string arguments = IsServerBuild
+					? $"-f \"{configFile}\" -r \"Dashboard\" -r \"cleartext\""
+					: $"-f \"{configFile}\" -r \"cleartext\"";
+
+				string executable = EnvironmentInfo.IsWin ? "dotnet-stryker.exe" : "dotnet-stryker";
+				IProcess process = ProcessTasks.StartProcess(
+						Path.Combine(toolPath, executable),
+						arguments,
+						Solution.Directory)
+					.AssertWaitForExit();
+				if (process.ExitCode != 0)
+				{
+					Assert.Fail(
+						$"Stryker did not execute successfully for {project.Key.Name}: (exit code {process.ExitCode}).");
+				}
+			}
+		});
+
+	static string PathForJson(Project project) => $"\"{project.Path.ToString().Replace(@"\", @"\\")}\"";
+}

Source/aweXpect.Chronology/Dummy.cs
@@ -1,0 +1,7 @@
+﻿namespace aweXpect.Chronology;
+
+public class Dummy
+{
+	public int Double(int value)
+		=> value * 2;
+}

Tests/aweXpect.Chronology.Api.Tests/ApiAcceptance.cs
@@ -1,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace aweXpect.Api.Tests;
+
+public sealed class ApiAcceptance
+{
+	/// <summary>
+	///     Execute this test to update the expected public API to the current API surface.
+	/// </summary>
+	[TestCase]
+	[Explicit]
+	public void AcceptApiChanges()
+	{
+		string[] assemblyNames =
+		[
+			"aweXpect.Chronology"
+		];
+
+		foreach (string assemblyName in assemblyNames)
+		{
+			foreach (string framework in Helper.GetTargetFrameworks())
+			{
+				string publicApi = Helper.CreatePublicApi(framework, assemblyName)
+					.Replace("\n", Environment.NewLine);
+				Helper.SetExpectedApi(framework, assemblyName, publicApi);
+			}
+		}
+
+		Assert.That(assemblyNames, Is.Not.Empty);
+	}
+}

Tests/aweXpect.Chronology.Api.Tests/ApiApprovalTests.cs
@@ -1,0 +1,38 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace aweXpect.Api.Tests;
+
+/// <summary>
+///     Whenever a test fails, this means that the public API surface changed.
+///     If the change was intentional, execute the <see cref="ApiAcceptance.AcceptApiChanges()" /> test to take over the
+///     current public API surface. The changes will become part of the pull request and will be reviewed accordingly.
+/// </summary>
+public sealed class ApiApprovalTests
+{
+	[TestCaseSource(typeof(TargetFrameworksTheoryData))]
+	public void VerifyPublicApiForAweXpectChronology(string framework)
+	{
+		const string assemblyName = "aweXpect.Chronology";
+
+		string publicApi = Helper.CreatePublicApi(framework, assemblyName);
+		string expectedApi = Helper.GetExpectedApi(framework, assemblyName);
+
+		Assert.That(publicApi, Is.EqualTo(expectedApi));
+	}
+
+	private sealed class TargetFrameworksTheoryData : IEnumerable
+	{
+		#region IEnumerable Members
+
+		public IEnumerator GetEnumerator()
+		{
+			foreach (string targetFramework in Helper.GetTargetFrameworks())
+			{
+				yield return new object[] { targetFramework };
+			}
+		}
+
+		#endregion
+	}
+}