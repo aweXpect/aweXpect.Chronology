abe65a94d5c59287628efc70b9f63c9ba260622d
Valentin <valentin.breuss@baur.eu>
2024-12-30 17:01:52 +0100

    Simplify UnitTest build

--- Pipeline/Build.UnitTest.cs
+++ Pipeline/Build.UnitTest.cs
@@ -1,11 +1,8 @@
+using System.Linq;
 using Nuke.Common;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.Xunit;
-using System.Linq;
-using static Nuke.Common.Tools.Xunit.XunitTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 // ReSharper disable AllUnderscoreLocalParameterName
@@ -15,7 +12,6 @@ namespace Build;
 partial class Build
 {
 	Target UnitTests => _ => _
-		.DependsOn(DotNetFrameworkUnitTests)
 		.DependsOn(DotNetUnitTests);
 
 	Project[] UnitTestProjects =>
@@ -23,39 +19,15 @@ partial class Build
 		Solution.Tests.aweXpect_Chronology_Tests
 	];
 
-	Target DotNetFrameworkUnitTests => _ => _
-		.Unlisted()
-		.DependsOn(Compile)
-		.OnlyWhenDynamic(() => EnvironmentInfo.IsWin)
-		.Executes(() =>
-		{
-			string net48 = "net48";
-			DotNetTest(s => s
-				.SetConfiguration(Configuration)
-				.SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
-				.EnableNoBuild()
-				.SetDataCollector("XPlat Code Coverage")
-				.SetResultsDirectory(TestResultsDirectory)
-				.CombineWith(
-					UnitTestProjects,
-					(settings, project) => settings
-						.SetProjectFile(project)
-						.CombineWith(
-							project.GetTargetFrameworks()?.Where(x => x == net48),
-							(frameworkSettings, framework) => frameworkSettings
-								.SetFramework(framework)
-								.AddLoggers($"trx;LogFileName={project.Name}_{framework}.trx")
-						)
-				), completeOnFailure: true
-			);
-		});
-
 	Target DotNetUnitTests => _ => _
 		.Unlisted()
 		.DependsOn(Compile)
 		.Executes(() =>
 		{
-			string net48 = "net48";
+			string[] excludedFrameworks =
+				EnvironmentInfo.IsWin
+					? []
+					: ["net48"];
 			DotNetTest(s => s
 					.SetConfiguration(Configuration)
 					.SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
@@ -67,7 +39,7 @@ partial class Build
 						(settings, project) => settings
 							.SetProjectFile(project)
 							.CombineWith(
-								project.GetTargetFrameworks()?.Except([net48]),
+								project.GetTargetFrameworks()?.Except(excludedFrameworks),
 								(frameworkSettings, framework) => frameworkSettings
 									.SetFramework(framework)
 									.AddLoggers($"trx;LogFileName={project.Name}_{framework}.trx")
