e82a85e16a560a886f6cc64a9b76d4646960bf1e
Valentin Breuß <vbreuss@gmail.com>
2025-01-11 11:32:20 +0100

    feat: add benchmarks (#22)
    Add benchmarks for creating a `TimeSpan`, `DateTime`, `DateOnly`, `TimeOnly` or `DateTimeOffset`.

---
+++ Benchmarks/aweXpect..Chronology.Benchmarks/HappyCaseBenchmarks.TimeOnly.cs
@@ -0,0 +1,16 @@
+﻿#if NET8_0_OR_GREATER
+using BenchmarkDotNet.Attributes;
+
+namespace aweXpect.Chronology.Benchmarks;
+
+public partial class HappyCaseBenchmarks
+{
+	[Benchmark]
+	public TimeOnly TimeOnly_aweXpect()
+		=> _hour.Hours(_minute.Minutes(_second.Seconds()));
+
+	[Benchmark]
+	public TimeOnly TimeOnly_DotNet()
+		=> TimeOnly.FromTimeSpan(new TimeSpan(_hour, _minute, _second));
+}
+#endif
---
+++ Benchmarks/aweXpect..Chronology.Benchmarks/HappyCaseBenchmarks.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace aweXpect.Chronology.Benchmarks;
+
+[MarkdownExporterAttribute.GitHub]
+[MemoryDiagnoser]
+public partial class HappyCaseBenchmarks
+{
+	private readonly int _day = 5;
+	private readonly int _hour = 14;
+	private readonly int _minute = 2;
+	private readonly int _month = 7;
+	private readonly int _second = 16;
+	private readonly int _year = 1964;
+}
